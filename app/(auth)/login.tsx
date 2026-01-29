f8393cf3ce17b052fe377d5d637f3a88e65e2f06
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-01-30 07:51:21 +0800

    feat: add register and login page testing

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,6 +1,42 @@
+import { AppButton } from "@/components/ui/appButton";
+import { AppInput } from "@/components/ui/appInput";
+import { useAuth } from "@/hooks/useAuth"
+import { Link } from "expo-router";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
 export default function login() {
-    const { register, handleSubmit, setValue } = useForm<LogIn>
+  const { login } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div>login</div>
+   <View style={styles.container}>
+    <Text style={styles.title}>Welcome Back</Text>
+    <AppInput placeholder="Email" value={email} onChangeText={setEmail}/>
+    <AppInput
+    placeholder="Password"
+    value={password}
+    onChangeText={setPassword}
+    secureTextEntry/>
+    <AppButton title="Login" onPress={() => login(email, password)}/>
+    <Link href={'/(auth)/register'}>Create an account</Link>
+   </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '700',
+    marginBottom: 24
+  },
+  link: {
+    marginTop: 16,
+    textAlign: 'center'
+  }
+})
