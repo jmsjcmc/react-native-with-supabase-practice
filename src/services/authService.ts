80797d44413e463c33b5118452153bb98dcc2194
James Jecemeco A. Tabilog <jecemecojamestabilog@gmail.com>
2026-01-31 21:54:20 +0800

    feat: add register supabase service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,3 +1,36 @@
-export const login = async (email: string, password: string) => {
-    const { data, error } = await supabase
+import { supabase } from "../supabase";
+
+export type RegisterRequest = {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+}
+
+export async function registerUser(input: RegisterRequest) {
+    const { email, password, firstName, lastName, phoneNumber} = input;
+
+    const { data: signUpRequest, error: signUpError } = 
+    await supabase.auth.signUp({
+        email, password
+    });
+
+    if (signUpError) throw signUpError;
+    if (!signUpRequest.user) throw new Error('User creation failded');
+
+    const { error: profileError } = await supabase
+    .from('profiles')
+    .insert({
+        id: signUpRequest.user?.id,
+        email,
+        firstName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber ?? null,
+        role: 'user'
+    });
+
+    if (profileError) throw profileError;
+
+    return signUpRequest.user;
 }
